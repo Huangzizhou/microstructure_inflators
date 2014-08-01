df1f5f5938a7a77f7d3e799a7f667fc69222f827
Julian Panetta <julian.panetta@gmail.com>
2014-08-01 18:03:23 -0400

    Improved the example script and added James' example meshes/boundary conditions.

--- material_optimization/cpp_version/matopt_flipper/matopt_run_example.sh
+++ material_optimization/cpp_version/matopt_flipper/matopt_run_example.sh
@@ -1,21 +1,25 @@
+#!/usr/bin/env zsh
 # Example of how to run a sweep over parameters of the material optimization
 # code on various inputs.
+# Assumes the MATOPT_DIR variable holds the name of the cpp material
+# optimization directory
+[[ -d $1 ]] || { echo "Usage: matopt_run_example.sh example_dir"; exit -1; }
+mesh_dir=$1;
 total=0.0;
 for regWeight in {10,100,1000,10000}; do
     dir=mtest_$regWeight
     mkdir $dir;
-    cd $dir;
-    for size in {8x8,16x16,32x32}; do
-        for cond in {tilt,compress,full_period,half_period}; do
+    for mesh in $mesh_dir/*.obj; do
+        for cond in ${mesh%.obj}*.bc; do
+            name="${cond%bc}"
             startTime=`perl -MTime::HiRes -e 'printf("%.0f\n",Time::HiRes::time()*1000)'`
-            ../TestMaterialOptimization2D ~/Downloads/mesh_bc/square_$size.obj ../box_bc/${cond}_$size.bc 15 $regWeight > ${cond}_$size.txt;
-            mv mtest.msh ${cond}_$size.msh;
+            $MATOPT_DIR/TestMaterialOptimization2D $mesh $cond 15 $regWeight > $dir/$(basename $name).txt
+            mv mtest.msh $dir/$(basename $name).msh;
             endTime=`perl -MTime::HiRes -e 'printf("%.0f\n",Time::HiRes::time()*1000)'`
             elapsed=$((($endTime - $startTime) / 1000.0));
             total=$(($total + $elapsed))
-            echo "${cond}_$size:\t" $(printf "%0.3fs" $elapsed);
+            echo "$name:\t" $(printf "%0.3fs" $elapsed);
         done;
     done
-    cd ..;
 done
 echo "Total time:\t$total";
