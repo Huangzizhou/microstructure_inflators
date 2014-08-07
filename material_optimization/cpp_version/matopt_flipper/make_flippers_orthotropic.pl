b86555a95d14d48e207213fc27c7d0e6bf1e6f13
Julian Panetta <julian.panetta@gmail.com>
2014-08-07 02:17:17 -0400

    3D material optimization scripts and examples.

--- material_optimization/cpp_version/matopt_flipper/make_flippers_orthotropic.pl
+++ material_optimization/cpp_version/matopt_flipper/make_flippers_orthotropic.pl
@@ -3,20 +3,30 @@
 # specified path, writing a flipper directory to directory.js.
 # The .txt file is the STDOUT of the cpp material optimization code, and the
 # .msh is its output msh file.
-# Usage: make_flippers_orthotropic_2D.pl [path]
+# Usage: make_flippers_orthotropic.pl dimension [path]
 use strict;
 use Cwd qw(abs_path cwd);
 use File::Basename;
 use Sort::Key::Natural 'natsort';
 my $scriptDir = abs_path(dirname(__FILE__));
 
+(@ARGV < 1 || @ARGV > 2) && die("Usage: make_flippers_orthotropic.pl dimension [directory]\n");
+
+my $dim = $ARGV[0];
+($dim ~~ [2, 3]) || die('Dimension must be 2 or 3.');
+
 chdir((@ARGV >= 1) ? @ARGV[0] : cwd());
+
+my $properties = ($dim == 2) ? 'E_x E_y nu_yx mu'
+         : 'E_x E_y E_z nu_yx nu_zx nu_zy mu_yz mu_zx mu_xy';
+my $commaProperties = $properties;
+$commaProperties =~ s/ /, /g;
 
 unlink 'directory.js';
 my @names;
 while (<*.msh>) {
     s/\.[^.]+$//;
-    `$scriptDir/make_flipper.pl $_.txt $_.msh $_ > $_.js 'E_x E_y nu_yx mu' 'E_x, E_y, nu_xy, mu'`;
+    `$scriptDir/make_flipper.pl $_.txt $_.msh $_ > $_.js $dim $properties $commaProperties'`;
     push(@names, $_);
 }
 
