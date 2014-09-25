f872a24cf7cf0da9c3a120c9a1d90355a3fdbc4a
Qingnan Zhou <qnzhou@gmail.com>
2014-09-25 10:01:18 -0400

    Minor update to get inflate.py working again.

--- wire_generator/inflate.py
+++ wire_generator/inflate.py
@@ -1,12 +1,18 @@
 #!/usr/bin/env python
 import argparse
+import numpy as np
 
-from WireNetwork import WireNetwork
-from WireInflator import WireInflator
+from core.WireNetwork import WireNetwork
+from inflator.WireInflator import WireInflator
 
-def inflate(wire_file, thickness, mesh_file):
+def inflate(wire_file, thickness, mesh_file, with_symmetry_orbits):
     wires = WireNetwork();
     wires.load_from_file(wire_file);
+    wires.attributes["vertex_thickness"] = \
+            np.ones(wires.num_vertices) * thickness;
+    if with_symmetry_orbits:
+        wires.attributes.add("symmetry_vertex_orbit");
+        wires.attributes.add("symmetry_edge_orbit");
     inflator = WireInflator(wires);
     inflator.inflate(thickness);
     inflator.save(mesh_file);
@@ -16,13 +22,16 @@ def parse_args():
     parser.add_argument("--thickness", "-t", help="target wire thickness",
             required=True, type=float);
     parser.add_argument("--output", "-o", help="output mesh file", required=True);
+    parser.add_argument("--with-symmetry-orbits",
+            help="output also symmetry orbits", action="store_true");
     parser.add_argument("wire_file", help="input .wire file");
     args = parser.parse_args();
     return args;
 
 def main():
     args = parse_args();
-    inflate(args.wire_file, args.thickness, args.output);
+    inflate(args.wire_file, args.thickness, args.output,
+            args.with_symmetry_orbits);
 
 if __name__ == "__main__":
     main();
