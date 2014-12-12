1d222c2be1004b96cff7b2408862549794cfa2ab
Qingnan Zhou <qnzhou@gmail.com>
2014-12-11 21:28:12 -0500

    Added capibility of generating non-cube boxes.

--- orthotropic_fit/box_gen.py
+++ orthotropic_fit/box_gen.py
@@ -8,10 +8,10 @@ from mesh_io import save_mesh, load_mesh
 
 from BoxMeshGenerator import generate_box_mesh
 
-def generate_box(dim, side_length, num_samples, output_name, keep_symmetry,
+def generate_box(side_lengths, num_samples, output_name, keep_symmetry,
         subdiv_order):
-    box_min = -0.5 * np.ones(dim) * side_length;
-    box_max =  0.5 * np.ones(dim) * side_length;
+    box_min = -0.5 * side_lengths;
+    box_max =  0.5 * side_lengths;
     mesh, material_index = generate_box_mesh(box_min, box_max, num_samples, keep_symmetry,
             subdiv_order);
     index_name = "cell_index"
@@ -26,6 +26,9 @@ def parse_args():
     parser.add_argument("--dim", help="mesh dimention", choices=[2, 3],
             default=3, type=int);
     parser.add_argument("--size", help="box size", type=float, default=10);
+    parser.add_argument("-X", help="X size", type=float, default=None);
+    parser.add_argument("-Y", help="Y size", type=float, default=None);
+    parser.add_argument("-Z", help="Z size", type=float, default=None);
     parser.add_argument("--num-samples",
             help="number of samples along each dimention", type=int, default=2);
     parser.add_argument("--subdiv", help="subdivision order", type=int,
@@ -36,7 +39,15 @@ def parse_args():
 
 def main():
     args = parse_args();
-    generate_box(args.dim, args.size, args.num_samples, args.output,
+    side_lengths = np.ones(args.dim) * args.size;
+    if (args.X is not None):
+        side_lengths[0] = args.X
+    if (args.Y is not None):
+        side_lengths[1] = args.Y
+    if (args.dim == 3 and args.Z is not None):
+        side_lengths[2] = args.Z
+
+    generate_box(side_lengths, args.num_samples, args.output,
             args.symmetric, args.subdiv);
 
 if __name__ == "__main__":
