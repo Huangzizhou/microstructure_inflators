35aa9e9d5ff098780b88586862e4343f00ec8503
Qingnan Zhou <qnzhou@gmail.com>
2014-12-19 11:18:29 -0500

    Update inflate.py to use PyWires inflator.

--- wire_generator/inflate.py
+++ wire_generator/inflate.py
@@ -1,11 +1,25 @@
 #!/usr/bin/env python
 import argparse
 import numpy as np
+import os.path
 
 from core.WireNetwork import WireNetwork
 from inflator.WireInflator import WireInflator
+from inflator.PyWiresInflator import PyWiresInflator
 
-def inflate(wire_file, thickness, mesh_file, with_symmetry_orbits, subdiv_order):
+import PyMesh
+
+def save_mesh(mesh_file, mesh):
+    basename, ext = os.path.splitext(mesh_file);
+    writer = PyMesh.MeshWriter.create_writer(mesh_file);
+    if ext in (".msh", ".ply"):
+        attr_names = mesh.get_attribute_names();
+        for name in attr_names:
+            writer.with_attribute(name);
+    writer.write_mesh(mesh);
+
+def inflate(wire_file, thickness, mesh_file, with_symmetry_orbits, subdiv_order,
+        subdiv_method):
     wires = WireNetwork();
     wires.load_from_file(wire_file);
     wires.attributes["vertex_thickness"] = \
@@ -13,9 +27,10 @@ def inflate(wire_file, thickness, mesh_file, with_symmetry_orbits, subdiv_order)
     if with_symmetry_orbits:
         wires.attributes.add("symmetry_vertex_orbit");
         wires.attributes.add("symmetry_edge_orbit");
-    inflator = WireInflator(wires);
-    inflator.inflate(thickness, subdiv_order, "loop");
-    inflator.save(mesh_file);
+    inflator = PyWiresInflator(wires, []);
+    inflator.inflate(True, subdiv_order, subdiv_method);
+    mesh = inflator.mesh;
+    save_mesh(mesh_file, mesh);
 
 def parse_args():
     parser = argparse.ArgumentParser(description="Inflate a wire network");
@@ -24,6 +39,8 @@ def parse_args():
     parser.add_argument("--output", "-o", help="output mesh file", required=True);
     parser.add_argument("--subdiv", "-s", type=int, default=0,
             help="number of subdivisions");
+    parser.add_argument("--subdiv-method", help="subdivision mehtod",
+            choices=["simple", "loop"], default="simple");
     parser.add_argument("--with-symmetry-orbits",
             help="output also symmetry orbits", action="store_true");
     parser.add_argument("wire_file", help="input .wire file");
@@ -33,7 +50,7 @@ def parse_args():
 def main():
     args = parse_args();
     inflate(args.wire_file, args.thickness, args.output,
-            args.with_symmetry_orbits, args.subdiv);
+            args.with_symmetry_orbits, args.subdiv, args.subdiv_method);
 
 if __name__ == "__main__":
     main();
