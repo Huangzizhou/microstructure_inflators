9f331349ff980d99cca46708b75969e875e1e781
Qingnan Zhou <qnzhou@gmail.com>
2015-01-14 10:52:22 -0500

    Added support for geometry correction.

--- parameter_lookup/generate_config.py
+++ parameter_lookup/generate_config.py
@@ -5,12 +5,14 @@ import json
 import os.path
 
 def output_config_file(wire_list_file, guide_mesh_file, thickness_type,
-        dof_type, output_file):
+        dof_type, output_file, spread_const, abs_geometry_correction):
     root_dir = os.path.dirname(output_file);
     wire_list_file = os.path.relpath(wire_list_file, root_dir);
     guide_mesh_file = os.path.relpath(guide_mesh_file, root_dir);
     config = {
             "wire_list_file": wire_list_file,
+            "geometry_spread": spread_const,
+            "abs_geometry_correction": abs_geometry_correction,
             "guide_mesh": guide_mesh_file,
             "subdiv": 0,
             "dof_type": dof_type,
@@ -25,6 +27,11 @@ def parse_args():
             default="edge");
     parser.add_argument("--dof-type", choices=("isotropic", "orthotropic"),
             default="isotropic");
+    parser.add_argument("--spread", help="spread constant", default=0,
+            type=float);
+    parser.add_argument("--abs-geometry-correction",
+            help="absolute geometry correction", nargs=3, type=float,
+            default=[0, 0, 0]);
     parser.add_argument("wire_list_file", help="wire list file");
     parser.add_argument("guide_mesh_file", help="guide mesh file");
     parser.add_argument("output_file", help="output config file");
@@ -37,7 +44,9 @@ def main():
             args.guide_mesh_file,
             args.thickness_type,
             args.dof_type,
-            args.output_file);
+            args.output_file,
+            args.spread,
+            args.abs_geometry_correction);
 
 if __name__ == "__main__":
     main();
