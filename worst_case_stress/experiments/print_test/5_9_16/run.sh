e9fce9fa2d1b190a4bea2e7b8e8eaea2a380d01f
Julian Panetta <julian.panetta@gmail.com>
2016-05-10 14:00:48 -0400

    Tweaks to 5_9_16 print scripts

--- worst_case_stress/experiments/print_test/5_9_16/run.sh
+++ worst_case_stress/experiments/print_test/5_9_16/run.sh
@@ -1,6 +1,7 @@
 ./gen_meshes.sh
 ./tile.sh
 ./extrude.sh
+./scale_extrusions.sh
 # ./gen_data_traction.sh | tee traction_experiment.dat
 # ./gen_data_dirichlet.sh | tee dirichlet_experiment.dat
 #  gnuplot plot.gpi
