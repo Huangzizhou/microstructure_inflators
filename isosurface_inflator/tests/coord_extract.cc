6b2f4f534759e6eb7822d8ce076cb9fa413bb12a
Julian Panetta <julian.panetta@gmail.com>
2016-05-17 19:05:40 -0400

    Added inflation debugging code

---
+++ isosurface_inflator/tests/coord_extract.cc
@@ -0,0 +1,21 @@
+#include <stdexcept>
+#include <iostream>
+#include <iomanip>
+#include <string>
+#include <MeshIO.hh>
+
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+    string path(argv[1]);
+    std::vector<MeshIO::IOVertex> vertices;
+    std::vector<MeshIO::IOElement> elements;
+    MeshIO::load(path, vertices, elements);
+
+    cout << setprecision(20);
+
+    for (const auto &v : vertices)
+        cout << v[0] << "\t" << v[1] << "\t" << v[2] << endl;
+    return 0;
+}
