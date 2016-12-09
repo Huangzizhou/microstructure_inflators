706420c2b0d0835fb49bbcca960ca6558d13b4cd
Julian Panetta <julian.panetta@gmail.com>
2016-12-09 16:07:59 -0500

    Added IGL-based mesher with parallelism.

---
+++ isosurface_inflator/IGLSurfaceMesherMC.hh
@@ -0,0 +1,83 @@
+#ifndef IGLSURFACEMESHERMC_HH
+#define IGLSURFACEMESHERMC_HH
+
+#include "MeshingOptions.hh"
+#include <MeshIO.hh>
+
+#include <igl/copyleft/marching_cubes.h>
+
+#include <Parallelism.hh>
+
+template<class SignedDistanceFunction>
+class IGLSurfaceMesherMC {
+public:
+    typedef typename SignedDistanceFunction::Real Real;
+    IGLSurfaceMesherMC(const MeshingOptions &opts = MeshingOptions())
+        : meshingOptions(opts) { }
+
+    void mesh(const SignedDistanceFunction &sdf,
+            std::vector<MeshIO::IOVertex> &vertices,
+            std::vector<MeshIO::IOElement> &elements,
+            const double isolevel = 0.0);
+
+    MeshingOptions meshingOptions;
+};
+
+template<class SignedDistanceFunction>
+void IGLSurfaceMesherMC<SignedDistanceFunction>::
+mesh(const SignedDistanceFunction &sdf,
+     std::vector<MeshIO::IOVertex> &vertices,
+     std::vector<MeshIO::IOElement> &elements, const double isolevel)
+{
+    size_t gs = meshingOptions.marchingCubesGridSize;
+    const auto bbox = sdf.boundingBox();
+
+    const size_t nsamples = gs * gs * gs;
+
+    // Evaluation point locations;
+    // flattened to be accessed as:
+    // xi + gs * (yi + gs * zi)
+    Eigen::MatrixXd sampleLocations(nsamples, 3);
+    {
+        size_t i = 0;
+        for (size_t zi = 0; zi < gs; ++zi) {
+            for (size_t yi = 0; yi < gs; ++yi) {
+                for (size_t xi = 0; xi < gs; ++xi) {
+                    sampleLocations.row(i) = bbox.interpolatePoint(
+                            Point3D(xi / Real(gs - 1.0),
+                                    yi / Real(gs - 1.0),
+                                    zi / Real(gs - 1.0)));
+                    ++i;
+                }
+            }
+        }
+    }
+
+    // Evaluate signed distances at each grid point
+    Eigen::VectorXd signedDistances(nsamples);
+#if USE_TBB
+    tbb::parallel_for(tbb::blocked_range<size_t>(0, nsamples),
+            [&](const tbb::blocked_range<size_t> &r) {
+                for (size_t i = r.begin(); i < r.end(); ++i)
+                    signedDistances(i) = sdf.signedDistance(sampleLocations.row(i)) - isolevel;
+            }
+        );
+#else
+    for (size_t i = 0; i < nsamples; ++i)
+        signedDistances(i) = sdf.signedDistance(sampleLocations.row(i)) - isolevel;
+#endif
+    Eigen::MatrixXd V;
+    Eigen::MatrixXi F;
+    igl::copyleft::marching_cubes(signedDistances, sampleLocations,
+            gs, gs, gs, V, F);
+
+    vertices.clear(), elements.clear();
+    vertices.reserve(V.rows());
+    elements.reserve(F.rows());
+    for (size_t i = 0; i < V.rows(); ++i)
+        vertices.emplace_back(Point3D(V.row(i)));
+    for (size_t i = 0; i < F.rows(); ++i)
+        elements.emplace_back(F(i, 0), F(i, 1), F(i, 2));
+}
+
+#endif /* end of include guard: IGLSURFACEMESHERMC_HH */
