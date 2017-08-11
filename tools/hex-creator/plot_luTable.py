59f15238b3cc1ef7386c8dda573f03b68cec38ae
Davi Colli Tozoni <davi.tozoni@gmail.com>
2017-08-11 17:52:51 -0400

    making it possible to choose material when plotting theoretical curve in plot_luTable.py. Also, adding option for opening both kinds of chart (poisson vs young and bulk vs shear)

--- tools/hex-creator/plot_luTable.py
+++ tools/hex-creator/plot_luTable.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env pythonw
 import itertools
 import json
+import os
 import random
 import sys
 
@@ -12,18 +13,24 @@ from matplotlib.patches import Patch
 
 import interactiveplotlib as ipl
 
-if len(sys.argv) < 3:
-    print "usage: ./plotLutTable.py type table"
-    print "example: ./plotLutTable.py type results_table1.txt results_table2.txt ..."
+if len(sys.argv) < 4:
+    print "usage: ./plotLutTable.py <type> <material> <table>"
+    print "example: ./plotLutTable.py type default-material results_table1.txt results_table2.txt ..."
     sys.exit(-1)
 
 dim = 2
-materialPath = '../../materials/B9Creator.material'
+
+materialPath = sys.argv[2]
+if os.path.isfile(materialPath):
+    print "Using material properties in: " + materialPath
+else:
+    materialPath = '../../materials/B9Creator.material'
+    print "Using default properties in: " + materialPath
+
 materialJson = json.load(file(materialPath))
 chart_type = sys.argv[1]
 baseE = materialJson['young']
 baseNu = materialJson['poisson']
-
 
 ax = plt.gca()
 ax.grid(True)
@@ -35,7 +42,7 @@ y = []
 annotes = []
 alpha = []
 p = []
-for i in range(2, len(sys.argv)):
+for i in range(3, len(sys.argv)):
     tablePath = sys.argv[i]
     tableFile = open(tablePath)
     for line in tableFile:
@@ -97,7 +104,7 @@ if chart_type == "triangle":
     af = ipl.AnnoteFinder(x, y, annotes, ax=ax)
     fig.canvas.mpl_connect('button_press_event', af)
 
-else:
+elif chart_type == "rectangle":
 
     # Now, create a new plot showing shear and bulk modulus
     x2 = []
@@ -131,6 +138,55 @@ else:
     af = ipl.AnnoteFinder(x2, y2, annotes, ax=ax)
     fig.canvas.mpl_connect('button_press_event', af)
 
+else:
+    fig, ax = plt.subplots()
 
-plt.legend(handles=legend_patches)
+    ax.grid(True)
+    triangle = hexlib.theoretical_triangle(baseE, baseNu, 1.0)
+    polygon = plt.Polygon(triangle, fill=None, color='r')
+    ax.add_patch(polygon)
+
+    ax.set_xlabel(r'$\nu$')
+    ax.set_ylabel('E')
+
+    col = ax.scatter(x, y, c='red', marker='+', picker=True)
+
+    af = ipl.AnnoteFinder(x, y, annotes, ax=ax, color='r', start_showing=False)
+    fig.canvas.mpl_connect('button_press_event', af)
+
+    # for index, note in enumerate(annotes):
+    #    ax.annotate(note, (x[index], y[index]))
+
+
+    # Now, create a new plot showing shear and bulk modulus
+    x2 = []
+    y2 = []
+    for index in range(0, len(x)):
+        nu = x[index]
+        E = y[index]
+
+        K = E / (2 * (1 - nu))
+        G = E / (2 * (1 + nu))
+
+        x2.append(K)
+        y2.append(G)
+
+    fig2, ax2 = plt.subplots()
+
+    # plt.axes()
+    ax2.grid(True)
+    rectangle = hexlib.theoretical_rectangle(baseE, baseNu, 1.0)
+    polygon = plt.Polygon(rectangle, fill=None, color='b')
+    ax2.add_patch(polygon)
+
+    # ax.axis('auto')
+    ax2.set_xlabel('$\kappa$')
+    ax2.set_ylabel(r'$\mu$')
+
+    col = ax2.scatter(x2, y2, c='blue', marker='o', picker=True)
+
+    af = ipl.AnnoteFinder(x2, y2, annotes, ax=ax2, color='b', start_showing=False)
+    fig2.canvas.mpl_connect('button_press_event', af)
+
+#plt.legend(handles=legend_patches)
 plt.show()
