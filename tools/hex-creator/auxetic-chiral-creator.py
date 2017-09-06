6631a62686628e0aaec99f833858906c3949210b
Davi Colli Tozoni <davi.tozoni@gmail.com>
2017-09-06 17:34:52 -0400

    implementation of simpler wire for auxetic hexagonal structures.

--- tools/hex-creator/auxetic-chiral-creator.py
+++ tools/hex-creator/auxetic-chiral-creator.py
@@ -42,7 +42,10 @@ def create_pillar_edge(edge):
         edge_intersection_point = hexlib.edge_intersection(edge, crossing_edge)
 
         if edge_intersection_point[0] <= (edge[1][0] + hexlib.Tolerance):
-            resulting_edge = [edge[0], edge_intersection_point]
+            if (edge[1][1] > 0.0):
+                resulting_edge = [edge[0], edge[1]]
+            else:
+                resulting_edge = [edge[0], edge_intersection_point]
         else:
             resulting_edge = edge
 
@@ -63,69 +66,42 @@ def create_pillars_with_constant_spacing_and_thickness(line1, line2, pillar_area
 
     edges_descriptions.append([line2[0], next_point2])
 
+    pillar_polygons = []
+    pillar_pairs = []
     for t in range(0, num_pillars):
         beginning_pillar1 = next_point1
+        midpoint_pillar1 = beginning_pillar1 + thickness * unit_vector / 2
         end_pillar1 = beginning_pillar1 + thickness * unit_vector
 
         beginning_pillar2 = next_point2
+        midpoint_pillar2 = beginning_pillar2 + thickness * unit_vector / 2
         end_pillar2 = beginning_pillar2 + thickness * unit_vector
 
-        left_edge = [beginning_pillar1, end_pillar1]
-        right_edge = [beginning_pillar2, end_pillar2]
-        top_edge = [beginning_pillar1, beginning_pillar2]
-        bottom_edge = [end_pillar1, end_pillar2]
+        left_edge1 = [beginning_pillar1, midpoint_pillar1]
+        left_edge2 = [midpoint_pillar1, end_pillar1]
 
-        resulting_left_edge = create_pillar_edge(left_edge)
-        resulting_right_edge = create_pillar_edge(right_edge)
-        resulting_top_edge = create_pillar_edge(top_edge)
-        resulting_bottom_edge = create_pillar_edge(bottom_edge)
+        right_edge1 = [beginning_pillar2, midpoint_pillar2]
+        right_edge2 = [midpoint_pillar2, end_pillar2]
 
-        pillar_edges = [resulting_left_edge, resulting_right_edge, resulting_top_edge, resulting_bottom_edge]
+        pillar_edge = [midpoint_pillar1, midpoint_pillar2]
 
-        # create triangles
-        if same_edges(top_edge, resulting_top_edge):
-            if same_edges(bottom_edge, resulting_bottom_edge):
-                pillar_edges.append(create_pillar_edge([beginning_pillar1, end_pillar2]))
+        resulting_left_edge1 = create_pillar_edge(left_edge1)
+        resulting_left_edge2 = create_pillar_edge(left_edge2)
+        resulting_right_edge1 = create_pillar_edge(right_edge1)
+        resulting_right_edge2 = create_pillar_edge(right_edge2)
+        resulting_pillar_edge = create_pillar_edge(pillar_edge)
 
-                triangles.append([beginning_pillar1, end_pillar2, end_pillar1])
-                triangles.append([beginning_pillar1, end_pillar2, beginning_pillar2])
+        pillar_polygons.append([beginning_pillar1, end_pillar1, end_pillar2, beginning_pillar2])
 
+        if not resulting_pillar_edge:
+            if not resulting_left_edge1 == []:
+                pillar_pairs.append([resulting_left_edge1[1], resulting_left_edge1[1]])
             else:
-                if not resulting_bottom_edge:
-
-                    pillar_edges.append([beginning_pillar1, [beginning_pillar2[0], 0.0]])
-                    pillar_edges.append([[end_pillar1[0], 0.0], [end_pillar2[0], 0.0]])
-
-                    triangles.append([beginning_pillar1, beginning_pillar2, [beginning_pillar2[0], 0.0]])
-                    triangles.append([beginning_pillar1, [beginning_pillar1[0], 0.0], [beginning_pillar2[0], 0.0]])
-                else:
-                    pillar_edges.append([beginning_pillar1, resulting_bottom_edge[1]])
-                    pillar_edges.append([resulting_bottom_edge[1], beginning_pillar2])
-                    pillar_edges.append([resulting_bottom_edge[1], [beginning_pillar2[0], 0.0]])
-
-                    triangles.append([beginning_pillar1, end_pillar1, resulting_bottom_edge[1]])
-                    triangles.append([beginning_pillar1, resulting_bottom_edge[1], beginning_pillar2])
-                    triangles.append([beginning_pillar2, resulting_bottom_edge[1], [beginning_pillar2[0], 0.0]])
+                pillar_pairs.append([])
         else:
-            if not resulting_top_edge:
-                pass
-            else:
-                if not resulting_bottom_edge:
-                    pillar_edges.append([resulting_top_edge[1], [beginning_pillar1[0], 0.0]])
+            pillar_pairs.append(resulting_pillar_edge)
 
-                    triangles.append([resulting_top_edge[1], [beginning_pillar1[0], 0.0], beginning_pillar1])
-                else:
-                    pillar_edges.append([beginning_pillar1, resulting_bottom_edge[1]])
-                    pillar_edges.append([resulting_top_edge[1], resulting_bottom_edge[1]])
-
-                    triangles.append([beginning_pillar1, resulting_bottom_edge[1], resulting_top_edge[1]])
-                    triangles.append([beginning_pillar1, end_pillar1, resulting_bottom_edge[1]])
-
-        # edges_descriptions.append(create_pillar_edge([beginning_pillar1, end_pillar1]))
-        # edges_descriptions.append(create_pillar_edge([beginning_pillar2, end_pillar2]))
-        # edges_descriptions.append(create_pillar_edge([beginning_pillar1, beginning_pillar2]))
-        # edges_descriptions.append(create_pillar_edge([end_pillar1, end_pillar2]))
-        # edges_descriptions.append(create_pillar_edge([beginning_pillar1, end_pillar2])) # create triangles
+        pillar_edges = [resulting_left_edge1, resulting_left_edge2, resulting_right_edge1, resulting_right_edge2, resulting_pillar_edge]
 
         next_point1 = end_pillar1 + gap * unit_vector
         next_point2 = end_pillar2 + gap * unit_vector
@@ -147,7 +123,7 @@ def create_pillars_with_constant_spacing_and_thickness(line1, line2, pillar_area
     else:
         edges_descriptions.append(final_edge)
 
-    return edges_descriptions, triangles
+    return edges_descriptions, triangles, pillar_polygons, pillar_pairs
 
 
 def subdivide_segment_with_constant_pillar_spacing_and_thickness(a, b, num_pillars, thickness, gap):
@@ -259,6 +235,17 @@ def simplex_polygon_to_whole_parallelogram(simplex_polygon, parallelogram_side):
 
     return polygons
 
+# Transforms vector into unit vector with same direction
+def unit_vector(vector):
+    return vector / math.sqrt(vector[0] ** 2 + vector[1] ** 2)
+
+
+# Computes angle between two vectors
+def min_angle_between(v1, v2):
+    v1_u = unit_vector(v1)
+    v2_u = unit_vector(v2)
+    return np.arccos(np.clip(np.dot(v1_u, v2_u), -1.0, 1.0))
+
 
 if len(sys.argv) != 7:
     print "usage: ./auxetic-chiral-creator.py <size of triangles> <num of pillars> <relative size of pillar area (chirality)> <relative thickness (of pillars)> " \
@@ -321,17 +308,19 @@ w3 = triangle_side_ratio * (b_yneg - c) + c
 
 print "Constructing " + out_wire + " ..."
 
-edge_descriptions, triangles = create_pillars_with_constant_spacing_and_thickness([q1, q3], [w1, w3], pillar_area_ratio,
+edge_descriptions, triangles, pillar_polygons, pillar_pairs = create_pillars_with_constant_spacing_and_thickness([q1, q3], [w1, w3], pillar_area_ratio,
                                                                                   num_pillars, thickness, spacing)
 
 # add edges to fill big inner hexagons
-edge_descriptions.append([a, q1])
-edge_descriptions.append([a, q2])
-edge_descriptions.append([c, w1])
-edge_descriptions.append([c, w2])
+q2_reflected = hexlib.reflect(30, [q2])[0]
 
-triangles.append([a, q1, q2])
-triangles.append([c, w1, w2])
+#edge_descriptions.append([a, q1])
+#edge_descriptions.append([a, q2])
+#edge_descriptions.append([c, w1])
+#edge_descriptions.append([c, w2])
+
+#triangles.append([a, q1, q2])
+#triangles.append([c, w1, w2])
 
 hexlib.add_new_edges(edge_descriptions, vertices, edges)
 
@@ -339,11 +328,84 @@ vertices, edges = simplex_to_whole_parallelogram(vertices, edges, parallelogram_
 
 vertices = vertices.tolist()
 
-parallelogram_triangles = []
-for triangle in triangles:
-    parallelogram_triangles += simplex_polygon_to_whole_parallelogram(triangle, parallelogram_side)
+#parallelogram_triangles = []
+#for triangle in triangles:
+#    parallelogram_triangles += simplex_polygon_to_whole_parallelogram(triangle, parallelogram_side)
 
-triangles_incenters_thickness_pairs = hexlib.add_polygons_incenters(parallelogram_triangles, vertices, edges)
+pillar_nodes_custom_pairs = []
+for index, polygon, in enumerate(pillar_polygons):
+    nodes = pillar_pairs[index]
+
+    print nodes
+
+    if len(nodes) == 0:
+        continue
+
+    transformed_polygons = simplex_polygon_to_whole_parallelogram(polygon, parallelogram_side)
+    transformed_pairs = simplex_polygon_to_whole_parallelogram(nodes, parallelogram_side)
+
+    for i, transformed_polygon in enumerate(transformed_polygons):
+        transformed_pair = transformed_pairs[i]
+
+        print "Uhul: " + str(transformed_pair)
+        nodes_thickness = hexlib.min_distance_point_line(transformed_pair[0], [transformed_polygon[0], transformed_polygon[3]])
+        pillar_nodes = [(transformed_polygon[0] + transformed_polygon[1])/2, (transformed_polygon[2] + transformed_polygon[3])/2]
+
+        pillar_nodes_custom_pairs.append([transformed_pair, nodes_thickness])
+        #print "Node thickness: " + str(nodes_thickness)
+
+
+triangles_incenters_thickness_pairs = [] #hexlib.add_polygons_incenters(parallelogram_triangles, vertices, edges)
+
+edge_descriptions = []
+left_quadrilateral = np.array([a, q2, q1, q2_reflected])
+transformation_matrix = np.matrix('1.0 -0.577350269189626; 0.0 1.154700538379251')
+resulting_quadrilateral = np.asarray((transformation_matrix * left_quadrilateral.transpose()).transpose() - 1.0)
+edge_descriptions.append([resulting_quadrilateral[0], resulting_quadrilateral[1]])
+edge_descriptions.append([resulting_quadrilateral[0], resulting_quadrilateral[2]])
+edge_descriptions.append([resulting_quadrilateral[0], resulting_quadrilateral[3]])
+origin_thickness = hexlib.min_distance_point_line(resulting_quadrilateral[0], [resulting_quadrilateral[1], resulting_quadrilateral[2]])
+left_quadrilateral_thickness_pairs = [[[resulting_quadrilateral[0]], origin_thickness]]
+hexlib.add_new_edges(edge_descriptions, vertices, edges)
+
+edge_descriptions = []
+right_quadrilateral = hexlib.reflect(135, resulting_quadrilateral)
+edge_descriptions.append([right_quadrilateral[0], right_quadrilateral[1]])
+edge_descriptions.append([right_quadrilateral[0], right_quadrilateral[2]])
+edge_descriptions.append([right_quadrilateral[0], right_quadrilateral[3]])
+reflected_origin_thickness = hexlib.min_distance_point_line(right_quadrilateral[0], [right_quadrilateral[1], right_quadrilateral[2]])
+right_quadrilateral_thickness_pairs = [[[right_quadrilateral[0]], reflected_origin_thickness]]
+hexlib.add_new_edges(edge_descriptions, vertices, edges)
+
+edge_descriptions = []
+w1_reflected = hexlib.reflect(120, [w1 - [2*l, 0]])[0] + [2*l, 0]
+w2_reflected = hexlib.reflect(120, [w2 - [2*l, 0]])[0] + [2*l, 0]
+right_pentagon = np.array([c, w2, w1, w1_reflected, w2_reflected])
+resulting_right_pentagon= np.asarray((transformation_matrix * right_pentagon.transpose()).transpose() - 1.0)
+angle_w1 = min_angle_between(resulting_right_pentagon[1] - resulting_right_pentagon[2], resulting_right_pentagon[3] - resulting_right_pentagon[2])
+angle_w2 = min_angle_between(resulting_right_pentagon[2] - resulting_right_pentagon[3], resulting_right_pentagon[4] - resulting_right_pentagon[3])
+point_bisection_w1 = hexlib.rotate(angle_w1/2 * 180.0 / math.pi, [resulting_right_pentagon[1] - resulting_right_pentagon[2]])[0] + resulting_right_pentagon[2]
+point_bisection_w1_reflected = hexlib.rotate(angle_w2/2 * 180.0 / math.pi, [resulting_right_pentagon[2] - resulting_right_pentagon[3]])[0] + resulting_right_pentagon[3]
+incenter = hexlib.edge_intersection([resulting_right_pentagon[2], point_bisection_w1], [resulting_right_pentagon[3], point_bisection_w1_reflected])
+min_distance = hexlib.min_distance_point_line(incenter, [resulting_right_pentagon[2], resulting_right_pentagon[3]])
+right_pentagon_thickness_pairs = [[[incenter], min_distance]]
+for vertex in resulting_right_pentagon:
+    edge_descriptions.append([incenter, vertex])
+hexlib.add_new_edges(edge_descriptions, vertices, edges)
+
+edge_descriptions = []
+left_pentagon = hexlib.reflect(30, right_pentagon)
+resulting_left_pentagon= np.asarray((transformation_matrix * left_pentagon.transpose()).transpose() - 1.0)
+angle_w1 = min_angle_between(resulting_left_pentagon[1] - resulting_left_pentagon[2], resulting_left_pentagon[3] - resulting_left_pentagon[2])
+angle_w2 = min_angle_between(resulting_left_pentagon[2] - resulting_left_pentagon[3], resulting_left_pentagon[4] - resulting_left_pentagon[3])
+point_bisection_w1 = hexlib.rotate(-angle_w1/2 * 180.0 / math.pi, [resulting_left_pentagon[1] - resulting_left_pentagon[2]])[0] + resulting_left_pentagon[2]
+point_bisection_w1_reflected = hexlib.rotate(-angle_w2/2 * 180.0 / math.pi, [resulting_left_pentagon[2] - resulting_left_pentagon[3]])[0] + resulting_left_pentagon[3]
+incenter = hexlib.edge_intersection([resulting_left_pentagon[2], point_bisection_w1], [resulting_left_pentagon[3], point_bisection_w1_reflected])
+min_distance = hexlib.min_distance_point_line(incenter, [resulting_left_pentagon[2], resulting_left_pentagon[3]])
+left_pentagon_thickness_pairs = [[[incenter], min_distance]]
+for vertex in resulting_left_pentagon:
+    edge_descriptions.append([incenter, vertex])
+hexlib.add_new_edges(edge_descriptions, vertices, edges)
 
 hexlib.create_wire(vertices, edges, out_wire)
 
@@ -365,4 +427,4 @@ print "Thickness void: " + str(thickness_void)
 print "Minimum resolution: " + str(min_resolution)
 print "Chosen resolution: " + str(chosen_resolution)
 
-hexlib.inflate_hexagonal_box_smarter(out_wire, 0.00001, 0.00001, out_mesh, triangles_incenters_thickness_pairs, chosen_resolution)
+hexlib.inflate_hexagonal_box_smarter(out_wire, 0.00001, 0.0, out_mesh, triangles_incenters_thickness_pairs + pillar_nodes_custom_pairs + left_quadrilateral_thickness_pairs + left_pentagon_thickness_pairs + right_quadrilateral_thickness_pairs + right_pentagon_thickness_pairs, chosen_resolution)
