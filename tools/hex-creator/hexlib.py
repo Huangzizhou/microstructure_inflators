45083e8272ad517476579b4313cc92415140deca
Davi Colli Tozoni <davi.tozoni@gmail.com>
2017-11-24 13:58:09 -0500

    script for transforming hexa (positive) params into general params (offset, radius, blending

--- tools/hex-creator/hexlib.py
+++ tools/hex-creator/hexlib.py
@@ -3,7 +3,7 @@ import os
 import re
 import json
 import uuid
-from subprocess import call
+from subprocess import call, check_output
 
 import numpy as np
 
@@ -167,7 +167,9 @@ def add_new_edges(edges_descriptions, current_vertices, current_edges):
             current_vertices.append(vertex2)
 
         new_edge = [position1, position2]
-        assert not position1 == position2
+        if position1 == position2:
+            print("Warning: trying to add edge where both vertices are the same")
+            continue
 
         edge_position = find_edge_in_list(current_edges, new_edge)
         if edge_position == -1:
@@ -247,7 +249,7 @@ def inflate_hexagonal_box(input_path, vertices_thickeness, out_path, vertices=[]
     for node in hypotenuse_nodes:
         index = find_vertex_in_list(vertices, node)
         if index == -1:
-            print "Warning: no vertex in this position", node
+            print("Warning: no vertex in this position", node)
             continue
         else:
             # has to adjust the size because original equilateral triangle in the parallelogram cells is not equilateral
@@ -295,7 +297,7 @@ def extract_independent_vertices(vertices):
     independent_vertices = []
 
     # initialize vertice containing map between vertices and indep index
-    independent_vertex_indices = range(0, len(vertices))
+    independent_vertex_indices = list(range(0, len(vertices)))
 
     for index, vertex in enumerate(vertices):
         independent_vertex = independent_vertex_position(vertex)
@@ -319,7 +321,7 @@ def create_custom_meshing_file(resolution):
     original_name = 'refined-meshing_opts.json'
     coarsening = 2
 
-    print script_directory
+#    print(script_directory)
     original_name = script_directory + "/" + original_name
 
     with open(original_name) as original_file:
@@ -377,10 +379,17 @@ def generate_parameters(independent_vertices, default_thickness, default_blendin
             if index >= 0:
                 parameters[offset_thickeness + index] = custom_thickness
             else:
-                print "[Warning] vertex with customized thickness not found"
+                print("[Warning] vertex with customized thickness not found")
 
     return parameters
 
+
+def generate_vertices_parameters(vertices, vertices_thickness, vertices_bending, custom_thickness_pairs):
+    independent_vertices, independent_vertex_indices = extract_independent_vertices(vertices)
+
+    parameters = generate_parameters(independent_vertices, vertices_thickness, vertices_bending, custom_thickness_pairs)
+
+    return parameters
 
 def inflate_hexagonal_box_smarter(input_path, vertices_thickness, vertices_bending, out_path,
                                   custom_thickness_pairs=[], resolution=64):
@@ -406,6 +415,7 @@ def inflate_hexagonal_box_smarter(input_path, vertices_thickness, vertices_bendi
     parameters = generate_parameters(independent_vertices, vertices_thickness, vertices_bending, custom_thickness_pairs)
 
     parameters_string = ' '.join(str(param) for param in parameters)
+    print(parameters_string)
 
     parameters_file_path = os.path.splitext(input_path)[0] + '.param'
     parameters_file = open(parameters_file_path, "w")
@@ -418,8 +428,8 @@ def inflate_hexagonal_box_smarter(input_path, vertices_thickness, vertices_bendi
     cmd = [cwd + '/../../isosurface_inflator/isosurface_cli', '2D_doubly_periodic', input_path, '--paramsFile',
            parameters_file_path, '-m', custom_meshing_path, '--cheapPostprocessing' ,
            '-D', 'inflated.msh', '-R', 'replicated.msh', out_path]
-    #print cmd
-    call(cmd)
+#    (print cmd)
+    check_output(cmd)
 
     if os.path.isfile(out_path):
         open(out_path + '.done', 'a').close()
@@ -860,7 +870,197 @@ def compute_volume_info(experiment_type, p1, p2, p3, p4):
         total_volume = math.sqrt(3) / 3
         assert abs(volume_fraction - volume / total_volume) < Tolerance
 
-    print "Volume is: ", volume
-    print "Volume fraction is ", volume_fraction
+    print("Volume is: ", volume)
+    print("Volume fraction is ", volume_fraction)
 
     return volume, volume_fraction
+
+
+def generate_positive_poisson_topology_and_thickness_info(p1, p2, p4):
+    triangle_side_ratio = p1
+    num_pillars = p2
+    pillar_area_ratio = 1.0
+    thickness_ratio = p4
+    parallelogram_side = 2.0
+    l = parallelogram_side / 2.0
+
+    triangle_side = triangle_side_ratio * 2
+    thickness = thickness_ratio * (pillar_area_ratio * triangle_side / num_pillars) / 2  # use the radius
+    # graph constructed on left triangle
+    vertices = []
+    edges = []
+
+    # define important vertices of parallelogram
+    origin = np.array([0, 0])
+    triangle_centroid = np.array([l, l * math.sqrt(3) / 3.0])
+
+    # First side: create pillars of the left side (counter-clock orientation)
+    #     /.
+    #    /  .
+    #   /.....
+    parallelogram_start = np.array([(l + triangle_side / 2.0) / 2.0, (l + triangle_side / 2.0) * math.sqrt(3) / 2.0])
+    parallelogram_end = np.array([(l - triangle_side / 2.0) / 2.0, (l - triangle_side / 2.0) * math.sqrt(3) / 2.0])
+
+    radius = triangle_side * math.sqrt(3) / 6
+    f = triangle_centroid - [0, radius]
+    triangle_start = f + [0, math.sqrt(3) * triangle_side / 2.0]
+    triangle_end = f - [triangle_side / 2.0, 0]
+
+    unit_vector = (parallelogram_end - parallelogram_start) / np.linalg.norm(parallelogram_end - parallelogram_start)
+    unit_vector_triangle = (triangle_end - triangle_start) / np.linalg.norm(triangle_end - triangle_start)
+
+    parallelogram_start_offset = parallelogram_start + thickness * unit_vector
+    parallelogram_end_offset = parallelogram_end - thickness * unit_vector
+    triangle_start_offset = triangle_start + thickness * unit_vector
+    triangle_end_offset = triangle_end - thickness * unit_vector
+
+    # add new vertices and edges to current sets
+    new_edges_description = create_pillars([parallelogram_start_offset, parallelogram_end_offset],
+                                                  [triangle_start_offset, triangle_end_offset], num_pillars)
+
+    # save pillar nodes
+    pillar_nodes = []
+    for edge_description in new_edges_description:
+        pillar_nodes.append(edge_description[0])
+        pillar_nodes.append(edge_description[1])
+
+    # new_edges_description += [[triangle_start, triangle_start_offset], [triangle_end, triangle_end_offset]]
+    add_new_edges(new_edges_description, vertices, edges)
+
+    # Second side: create pillars of the bottom side (counter-clock orientation)
+    #     ..
+    #    .  .
+    #   ______
+    parallelogram_start = np.array([l - triangle_side / 2.0, 0])
+    parallelogram_end = np.array([l + triangle_side / 2.0, 0])
+
+    triangle_start = f - [triangle_side / 2.0, 0]
+    triangle_end = f + [triangle_side / 2.0, 0]
+
+    pillar_example = [[parallelogram_start, triangle_start]]
+
+    unit_vector = (parallelogram_end - parallelogram_start) / np.linalg.norm(parallelogram_end - parallelogram_start)
+    unit_vector_triangle = (triangle_end - triangle_start) / np.linalg.norm(triangle_end - triangle_start)
+
+    parallelogram_start_offset = parallelogram_start + thickness * unit_vector
+    parallelogram_end_offset = parallelogram_end - thickness * unit_vector
+    triangle_start_offset = triangle_start + thickness * unit_vector
+    triangle_end_offset = triangle_end - thickness * unit_vector
+
+    # add new vertices and edges to current sets
+    new_edges_description = create_pillars([parallelogram_start_offset, parallelogram_end_offset],
+                                                  [triangle_start_offset, triangle_end_offset],
+                                                  num_pillars)
+
+    # save pillar nodes
+    for edge_description in new_edges_description:
+        pillar_nodes.append(edge_description[0])
+        pillar_nodes.append(edge_description[1])
+
+    # new_edges_description += [[triangle_start, triangle_start_offset], [triangle_end, triangle_end_offset]]
+    add_new_edges(new_edges_description, vertices, edges)
+
+    # Third side: create pillars of the right side (counter-clock orientation)
+    #     .\
+    #    .  \
+    #   .....\
+    parallelogram_start = np.array(
+        [2 * l - (l - triangle_side / 2.0) / 2, (l - triangle_side / 2.0) * math.sqrt(3) / 2])
+    parallelogram_end = np.array([2 * l - (l + triangle_side / 2.0) / 2, (l + triangle_side / 2.0) * math.sqrt(3) / 2])
+
+    triangle_start = f + [triangle_side / 2.0, 0]
+    triangle_end = f + [0, math.sqrt(3) * triangle_side / 2.0]
+
+    unit_vector = (parallelogram_end - parallelogram_start) / np.linalg.norm(parallelogram_end - parallelogram_start)
+    unit_vector_triangle = (triangle_end - triangle_start) / np.linalg.norm(triangle_end - triangle_start)
+
+    parallelogram_start_offset = parallelogram_start + thickness * unit_vector
+    parallelogram_end_offset = parallelogram_end - thickness * unit_vector
+    triangle_start_offset = triangle_start + thickness * unit_vector
+    triangle_end_offset = triangle_end - thickness * unit_vector
+
+    # add new vertices and edges to current sets
+    new_edges_description = create_pillars([parallelogram_start_offset, parallelogram_end_offset],
+                                                  [triangle_start_offset, triangle_end_offset],
+                                                  num_pillars)
+
+    # save nodes on hypotenuse
+    hypotenuse_nodes = []
+    for edge_description in new_edges_description:
+        hypotenuse_nodes.append(edge_description[0])
+        hypotenuse_nodes.append(edge_description[1])
+
+    # new_edges_description += [[triangle_start, triangle_start_offset], [triangle_end, triangle_end_offset]]
+    add_new_edges(new_edges_description, vertices, edges)
+
+    # Now, adding edges on the triangle
+    triangle = [f + [0, math.sqrt(3) * triangle_side / 2.0], f - [triangle_side / 2.0, 0], f + [triangle_side / 2.0, 0]]
+    new_edges_description = create_triangle_edges(triangle, num_pillars, thickness, thickness)
+    add_new_edges(new_edges_description, vertices, edges)
+
+    # Now, transform to square every vertex we have:
+    vertices = np.array(vertices)
+    transformation_matrix = np.matrix('1.0 -0.577350269189626; 0.0 1.154700538379251')
+    resulting_vertices = transformation_matrix * vertices.transpose()
+    vertices = np.asarray(resulting_vertices.transpose())
+
+    # Finally, transpose to origin and reflect through the diagonal 'y = -x'
+    vertices -= 1.0
+
+    reflected_vertices = vertices[:, [1, 0]] * (-1)
+
+    # saving vertices on hypotenuses
+    hypotenuse_nodes = np.array(hypotenuse_nodes)
+    resulting_hypotenuse_nodes = transformation_matrix * hypotenuse_nodes.transpose()
+    hypotenuse_nodes = np.asarray(resulting_hypotenuse_nodes.transpose())
+    hypotenuse_nodes -= 1.0
+    reflected_hypotenuse_nodes = hypotenuse_nodes[:, [1, 0]] * (-1)
+    hypotenuse_nodes = hypotenuse_nodes.tolist() + reflected_hypotenuse_nodes.tolist()
+
+    # saving vertices on pillars
+    pillar_nodes = np.array(pillar_nodes)
+    resulting_pillar_nodes = transformation_matrix * pillar_nodes.transpose()
+    pillar_nodes = np.asarray(resulting_pillar_nodes.transpose())
+    pillar_nodes -= 1.0
+    reflected_pillar_nodes = pillar_nodes[:, [1, 0]] * (-1)
+    pillar_nodes = pillar_nodes.tolist() + reflected_pillar_nodes.tolist()
+
+    # transforming pillar example
+    pillar_example = np.array(pillar_example)
+    resulting_pillar_example = transformation_matrix * pillar_example.transpose()
+    pillar_example = np.asarray(resulting_pillar_example.transpose())
+    delta_y = abs(pillar_example[1][1] - pillar_example[0][1])
+    norm_example = np.linalg.norm(pillar_example[1] - pillar_example[0])
+    thickness_correction_factor = delta_y / norm_example
+    # print("Thickness correction factor: " + str(thickness_correction_factor))
+
+    new_edges_description = []
+    for edge in edges:
+        v1 = edge[0]
+        v2 = edge[1]
+
+        reflected_vertex1 = reflected_vertices[v1]
+        reflected_vertex2 = reflected_vertices[v2]
+
+        new_edges_description.append([reflected_vertex1, reflected_vertex2])
+
+    vertices = vertices.tolist()
+    add_new_edges(new_edges_description, vertices, edges)
+
+    # triangle is also transformed and reflected
+    triangle = np.array(triangle)
+    resulting_triangle = transformation_matrix * triangle.transpose()
+    transformed_triangle = np.asarray(resulting_triangle.transpose())
+
+    transformed_triangle -= 1.0
+    reflected_triangle = transformed_triangle[:, [1, 0]] * (-1)
+
+    incenter_triangle_pairs = add_polygons_incenters([transformed_triangle, reflected_triangle], vertices, edges)
+
+    custom_thickness_pairs = incenter_triangle_pairs + [[hypotenuse_nodes, float(thickness) * math.sqrt(2)],
+                               [pillar_nodes, float(thickness) * thickness_correction_factor]]
+
+    return vertices, edges, custom_thickness_pairs
+
+
+
