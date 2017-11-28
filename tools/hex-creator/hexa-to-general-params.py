5dcfd04c35024b78193b09c60d0d762964fadc64
Davi Colli Tozoni <davi.tozoni@gmail.com>
2017-11-28 01:17:44 -0500

    adding results for extreme poisson ratio structure + leaving convertion script with the 4 original parameters (adding p3)

--- tools/hex-creator/hexa-to-general-params.py
+++ tools/hex-creator/hexa-to-general-params.py
@@ -3,14 +3,15 @@ import sys
 
 import hexlib
 
-if len(sys.argv) != 4:
-    print("usage: ./hexa-to-general-params.py <size of triangles (p1)> <num of pillars (p2)> <relative thickness (of pillars) (p4)>")
-    print("example: ./hexa-to-general-params.py 0.8 3 0.9")
+if len(sys.argv) != 5:
+    print("usage: ./hexa-to-general-params.py <size of triangles (p1)> <num of pillars (p2)> <chirality (p3)> <relative thickness (of pillars) (p4)>")
+    print("example: ./hexa-to-general-params.py 0.8 3 1.0 0.9")
     sys.exit(-1)
 
 p1 = float(sys.argv[1])
 p2 = int(sys.argv[2])
-p4 = float(sys.argv[3])
+p3 = float(sys.argv[3])
+p4 = float(sys.argv[4])
 
 vertices, edges, custom_pairs = hexlib.generate_positive_poisson_topology_and_thickness_info(p1, p2, p4)
 
