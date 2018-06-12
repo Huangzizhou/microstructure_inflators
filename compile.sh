e3645f330a1776d56e30ef829594d1113a5c6682
Jérémie Dumas <jeremie.dumas@ens-lyon.org>
2018-06-12 18:47:58 -0400

    Job file for compilation on HPC.

---
+++ compile.sh
@@ -0,0 +1,54 @@
+#! /bin/bash
+#
+# Submit job as (build defaults to Release):
+#
+#   sbatch compile.sh
+#   sbatch --export=BUILD='Debug',ALL compile.sh
+#
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=8
+#SBATCH --time=2:00:00
+#SBATCH --mem=16GB
+
+# Load modules
+module purge
+module load mercurial/intel/4.0.1
+module load gcc/6.3.0
+module load cmake/intel/3.7.1
+
+export CC=${GCC_ROOT}/bin/gcc
+export CXX=${GCC_ROOT}/bin/g++
+
+# Microstructure code
+module load tbb/intel/2017u3
+module load suitesparse/intel/4.5.4
+module load boost/intel/1.62.0
+module load cgal/intel/4.10b1
+module load glog/intel/0.3.4
+
+# Run job
+cd "${SLURM_SUBMIT_DIR}"
+
+# Prepare external libraries (Ceres ...)
+pushd 3rdparty
+mkdir build
+cd build
+cmake ..
+make -j8
+popd
+
+# Compile main program
+mkdir build
+cd build
+echo ${BUILD}
+
+if [ -z "${BUILD}" ]; then
+	BUILD=Release
+fi
+
+mkdir ${BUILD}
+pushd ${BUILD}
+cmake -DCMAKE_BUILD_TYPE=${BUILD} ../..
+make -j8
+popd
