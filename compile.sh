de4c9ee925216d7fa1c78bc9caa54364013d95bc
dtozoni <davi.tozoni@gmail.com>
2019-05-14 10:08:09 -0400

    adding mpfr and gmp loading to compilation script

--- compile.sh
+++ compile.sh
@@ -16,6 +16,11 @@ module purge
 module load mercurial/intel/4.0.1
 module load gcc/6.3.0
 module load cmake/intel/3.7.1
+module load mpfr/gnu/3.1.5
+module load gmp/gnu/6.1.2
+
+export GMP_DIR=$GMP_ROOT
+export MPFR_DIR=$MPFR_ROOT
 
 export CC=${GCC_ROOT}/bin/gcc
 export CXX=${GCC_ROOT}/bin/g++
@@ -30,6 +35,7 @@ cd "${SLURM_SUBMIT_DIR}"
 
 # Compile main program
 SOURCE_DIR=${SLURM_SUBMIT_DIR}
+#BUILD_DIR=build
 BUILD_DIR=/scratch/${USER}/build/microstructures
 
 mkdir -p ${BUILD_DIR}
